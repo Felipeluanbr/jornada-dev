2a7c921d83a07055537278ed137d9117b0d16ea1
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-02-18 07:36:05 -0300

    Operador switch

---
+++ JavaScript/condicionais/switchCase.js
@@ -0,0 +1,24 @@
+// Oparador switch
+
+const avaliacaoUsuario = 2;
+
+switch (avaliacaoUsuario) {
+  case 5:
+    console.log("avaliaçao e 5");
+    break;
+  case 4:
+    console.log("avaliaçao e 4");
+    break;
+
+    case 3:
+    console.log("avaliaçao e 3");
+    break;
+
+    case 2:
+    console.log("avaliaçao e 2");
+    break;
+
+  default:
+    console.log('avaliaçao e 1')
+    break;
+}
