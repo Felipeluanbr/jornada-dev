bbe73d3a104335d532f24888f604d08b7eb29673
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-02-19 06:30:25 -0300

    Laços de repetição

---
+++ JavaScript/laços-de-repeticao/for.js
@@ -0,0 +1,18 @@
+// Laço de repetiçao simples
+let numero = 5;
+
+for (let a = 1; a <= 10; a++) {
+  const resultado = numero * a;
+  console.log(`O numero ${numero} multiplicado por ${a} = ${resultado}`);
+}
+
+// Laço de repetiçao usando uma condiçao dentro dele
+
+
+for (let contador = 1; contador <= 50; contador++) {
+  const num2 = Math.floor(Math.random() * (50 - 1 + 1) + 1);
+  if(num2 === 15){
+    console.log(`${num2} em  ${contador} tentativas`);
+    break
+  }
+}
