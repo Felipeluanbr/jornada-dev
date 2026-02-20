95ffee02851ed50aba3e86f5c34e6a2b015717f9
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-02-20 06:38:21 -0300

    Exercicio while

---
+++ JavaScript/laços-de-repeticao/exerciciosWhile.js
@@ -0,0 +1,13 @@
+let palavraSecreta = 8;
+let palavraAleatoria = 0;
+
+while(palavraSecreta !== palavraAleatoria){
+    console.log('Condição é verdadeira');
+    palavraAleatoria++
+} 
+
+console.log('Parou!');
+
+
+
+
