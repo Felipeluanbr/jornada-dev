af1dcc9f6f4025bf6c94f89a941e10ec3b3879ce
Felipe Desenvolvedor <felipeluansilvacoelho2019@gmail.com>
2026-02-20 09:48:12 -0300

    Corrigindo submodulo acidental

---
+++ JavaScript/lacos-de-repeticao/exerciciosDoWhile.js
@@ -0,0 +1,21 @@
+let palavraSecreta = 8;
+let palavraAleatoria = 0;
+let resultado = 0;
+
+do {
+    console.log('Está sendo executada');
+    resultado++;
+} while (palavraAleatoria < palavraSecreta);
+
+console.log('Executou', resultado, 'vez(es)');
+
+
+
+const texto = 'Felipe'
+
+console.log(texto.length);
+console.log(texto[5]);
+console.log(texto[4]);
+
+
+
